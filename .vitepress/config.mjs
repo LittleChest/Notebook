77e6883f31bb5e15aff115cdef33267059e3aab6
LittleChest <hiya@littlew.top>
2025-09-21 21:39:03 +0800

    vitepress: Theme config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -8,4 +8,26 @@ export default defineConfig({
   cleanUrls: true,
   srcDir: "docs",
   metaChunk: true,
+  themeConfig: {
+    outline: {
+      label: "目录",
+    },
+    editLink: {
+      pattern:
+        "https://github.com/LittleChest/Documention/edit/main/docs/:path",
+      text: "在 GitHub 上编辑此页面",
+    },
+    lastUpdated: {
+      text: "最后更新于",
+    },
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+    lightModeSwitchTitle: "切换到浅色模式",
+    darkModeSwitchTitle: "切换到深色模式",
+    sidebarMenuLabel: "菜单",
+    returnToTopLabel: "返回顶部",
+    langMenuLabel: "选择语言",
+  },
 });
